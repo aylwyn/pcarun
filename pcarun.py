93ee2e295d79bb66e6314f6a104f9c844d9b0070
aylwyn <aylwyn@cantab.net>
2014-04-25 11:11:12 +0100

    multiple changes

--- pcarun.py
+++ pcarun.py
@@ -12,7 +12,7 @@ from logging import error, warning, info, debug, critical
 
 import aosutils
 
-# TODO: start from bam files
+EPREF = 'eig'
 
 def prep(args):
 	debug(args)
@@ -57,7 +57,6 @@ def prep(args):
 	if os.path.exists(maindir):
 		os.chdir(maindir)
 
-	outpref = 'eig'
 #		# make snp-geno dir
 #		subdir = 'snp'
 #		if not os.path.exists(subdir):
@@ -71,7 +70,12 @@ def prep(args):
 #		if os.path.exists(subdir):
 #			os.chdir(subdir)
 
-	# make segsep/psmcfa files
+	# make eigenstrat files
+	outfile = EPREF + '.ind'
+	if os.path.exists(outfile) and not (args.replace or args.sim):
+		error('%s exists; use replace' % outfile)
+		return(2)
+
 	if args.s1name == 'vcf':
 		if args.vcf_list and not (args.sim or os.path.exists('tmp')):
 			os.mkdir('tmp')
@@ -92,12 +96,10 @@ def prep(args):
 				reparg = '--replacecalls=%s' % tmprep
 			else:
 				reparg = ''
-			cmd = 'bsub.py "%s %s | vcf-proc.py -H --vars %s | varsites2eigenstrat.py --append -p %s" -o %s.out -M 2 -j %s' % (bcfview, vcf, reparg, outpref, outpref, jobname)
-			if args.replace:
-				cmd += ' --replace'
-			if args.bsim:
-				cmd += ' --sim'
-			info('submitting \'%s\'' % (jobname))
+#			cmd = 'bsub.py "%s %s | vcf-proc.py -H --vars %s | varsites2eigenstrat.py --append -p %s" -o %s.out -M 2 -j %s' % (bcfview, vcf, reparg, EPREF, EPREF, jobname)
+			cmd = '%s %s | vcf-proc.py -H --vars %s | varsites2eigenstrat.py --append -p %s' % (bcfview, vcf, reparg, EPREF)
+			info('processing %s' % (vcf))
+#			info('running \'%s\'' % (jobname))
 			aosutils.subcall(cmd, args.sim, wait = True)
 	elif args.s1name == 'ms':
 		jobname = ':'.join(('ms2eigenstrat', sname))
@@ -105,75 +107,45 @@ def prep(args):
 		info('running \'%s\'' % (jobname))
 		aosutils.subcall(cmd, args.sim, wait = True)
 
-def run(args): # run smc inference
+def run(args): # run pca
 	os.chdir(args.DIR)
 	debug('In %s:' % args.DIR)
-	if args.psmc:
-		psmcfaf = glob.glob('psmcfa/*.psmcfa')[0]
-		sname = 'psmc'
-		jobname = ':'.join(('psmc', args.DIR))
-		outf = '%s.out' % sname
-		if not args.memory:
-			args.memory = 3
-		#TODO: cat args.nfiles psmcfa files into all.psmfca
-		cmd = 'bsub.py "psmc -p \'%s\' %s" -o %s -M %d -j %s' % (args.intervals, psmcfaf, outf, args.memory, jobname)
-#		cmd = 'bsub.py "psmc -N25 -t15 -r5 -p \'%s\' %s" -o %s -M %d -j %s' % (args.intervals, psmcfaf, outf, args.memory, jobname)
-		if args.replace:
-			cmd += ' --replace'
-		if args.bsim:
-			cmd += ' --sim'
-		if os.path.exists(outf) and not args.replace:
-			warning('%s/%s exists; use --replace' % (args.DIR, outf))
-		else:
-			info('submitting \'%s\'' % (jobname))
-			aosutils.subcall(cmd, args.sim, wait = True)
+
+	if not args.sim:
+		parname = args.runpref + '.par'
+		parfile = open(parname, 'w')
+		parfile.write('genotypename:\t%s.geno\n' % EPREF)
+		parfile.write('snpname:\t%s.snp\n' % EPREF)
+		parfile.write('indivname:\t%s.ind\n' % EPREF)
+		parfile.write('evecoutname:\t%s.evec\n' % args.runpref)
+		parfile.write('evaloutname:\t%s.eval\n' % args.runpref)
+		parfile.write('numoutevec:\t%d\n' % args.numoutevec)
+		parfile.write('nsnpldregress:\t%d\n' % args.nsnpldregress)
+		parfile.write('numthreads:\t%d\n' % args.threads)
+		parfile.close()
+
+	outf = args.runpref + '.out'
+	jobname = ':'.join(('smartpca', args.runpref, os.path.basename(os.path.abspath(os.path.normpath(args.DIR)))))
+	if not args.memory:
+		args.memory = 10
+	cmd = 'bsub.py "smartpca -p %s" -o %s -M %d -t %d -q %s -j %s' % (parname, outf, args.memory, args.threads, args.queue, jobname)
+	if args.replace:
+		cmd += ' --replace'
+	if args.bsim:
+		cmd += ' --sim'
+	if os.path.exists(outf) and not args.replace:
+		warning('%s/%s exists; use --replace' % (args.DIR, outf))
 	else:
-		infiles = glob.glob('segsep/*.segsep')
-		infiles.sort(key=aosutils.natural_key)
-		if args.nfiles:
-			infiles = infiles[:args.nfiles]
-		debug(infiles)
-
-		if args.combined:
-			infiles = [' '.join(infiles)]
-
-		for jf, infile in enumerate(infiles):
-			if args.combined:
-				pref = 'combined_%d' % args.nfiles
-			else:
-				pref = os.path.basename(infile).replace('.segsep', '')
-			sname = '%s.msmc' % pref
-			outf = sname + '.out'
-			jobname = ':'.join(('msmc', os.path.basename(os.path.normpath(args.DIR)), pref))
-			if args.geneflow: # assume two samples
-				if not args.memory:
-					args.memory = 16
-				cmd = 'bsub.py "msmc --fixedRecombination -P 0,0,1,1 -p \'%s\' -t %d -o %s %s" -o %s -M %d -t %d -q %s -j %s' % (args.intervals, args.threads, sname, infile, outf, args.memory, args.threads, args.queue, jobname)
-			else:
-				if not args.memory:
-					args.memory = 10
-				cmd = 'bsub.py "msmc -t %d -o %s %s" -o %s -M %d -t %d -q %s -j %s' % (args.threads, sname, outf, infile, args.memory, args.threads, args.queue, jobname)
-			if args.replace:
-				cmd += ' --replace'
-			if args.bsim:
-				cmd += ' --sim'
-			if os.path.exists(outf) and not args.replace:
-				warning('%s/%s exists; use --replace' % (args.DIR, outf))
-			else:
-				info('submitting \'%s\'' % (jobname))
-				aosutils.subcall(cmd, args.sim, wait = True)
+		info('submitting \'%s\'' % (jobname))
+		aosutils.subcall(cmd, args.sim, wait = True)
 
 def plot(args): # make plots
 	if not args.sim:
 		os.chdir(args.DIR)
-#	sname = os.path.splitext(os.path.basename(os.path.normpath(args.DIR)))[0]
 	sname = os.path.basename(os.path.normpath(args.DIR))
 	jobname = ':'.join(('smcplot', sname))
 
-	if args.geneflow:
-		cmd = 'smcplot.py -m run.final.txt -t %f -u %e --geneflow --hc -o %s-geneflow' % (args.tgen, args.mu, sname)
-	else:
-		cmd = 'smcplot.py -m run.final.txt -t %f -u %e --hc -o %s' % (args.tgen, args.mu, sname)
+	cmd = 'smcplot.py -m run.final.txt -t %f -u %e --hc -o %s' % (args.tgen, args.mu, sname)
 	info('running \'%s\'' % (jobname))
 	aosutils.subcall(cmd, args.sim, wait = True)
 
@@ -204,21 +176,18 @@ p1.set_defaults(func=prep)
 
 p2 = s.add_parser('run', parents=[pp], help='run smartpca')
 p2.add_argument('DIR')
-#p2.add_argument('--geneflow', action='store_true', default = False, help = 'infer gene flow with msmc')
-#p2.add_argument('-p', '--intervals', default = '6*1+7*2', help = 'time interval specification (psmc format)')
-#p2.add_argument('--combined', action='store_true', default = False, help = 'run on all segsep files combined')
-#p2.add_argument('-n', '--nfiles', type=int, default=0, help = 'number of segsep files to include')
-p2.add_argument('-t', '--threads', type=int, default=4, help = 'number of threads to use')
+p2.add_argument('-p', '--runpref', default='eig', help='output file prefix') 
+p2.add_argument('--numoutevec', type=int, default=4, help = 'number of output eigenvectors (smartpca)')
+p2.add_argument('--nsnpldregress', type=int, default=0, help = 'number of LD regression SNPs (smartpca)')
+p2.add_argument('-t', '--threads', type=int, default=8, help = 'number of threads to use')
 p2.add_argument('-q', '--queue', default='normal', help = 'queue to use')
 p2.add_argument('-M', '--memory', type=int, default=0, help = 'GB of RAM to use')
 p2.set_defaults(func=run)
 
-p3 = s.add_parser('plot', parents=[pp], help='plot pca results')
-p3.add_argument('DIR')
-#p3.add_argument('--geneflow', action='store_true', default = False, help = 'infer gene flow with msmc')
-#p3.add_argument('-t', '--tgen', type=int, default=30.0, help = 'generation time (y)')
-#p3.add_argument('-u', '--mu', type=int, default=1.25e-8, help = 'per-generation mutation rate (bp^-1)')
-p3.set_defaults(func=plot)
+#p3 = s.add_parser('plot', parents=[pp], help='plot pca results')
+#p3.add_argument('DIR')
+##p3.add_argument('-t', '--tgen', type=int, default=30.0, help = 'generation time (y)')
+#p3.set_defaults(func=plot)
 
 args = p.parse_args()
 
